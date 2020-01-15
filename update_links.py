32c1e45796108292831dd9df9a67249f2fee7475
Aafrecc <aafrected@gmail.com>
2020-01-15 14:55:12 +0100

    Added some dotfiles.

--- update_links.py
+++ update_links.py
@@ -1,11 +1,20 @@
+#!/usr/bin/env python
+
+
 from subprocess import run
 from os import listdir
 
 config_files = [i for i in listdir('./.config')]
+dot_files = [i for i in listdir('./') if i[0] == '.' and i not in ['.git', '.config']]
 exec_files = [i for i in listdir('./scripts')]
 
-for i in config_files:
-    run(['ln', '-srf', './.config/{0}'.format(i) '~/.config/{0}'.format(i)])
+# for i in config_files:
+#     run(['ln', '-srf', './.config/{0}'.format(i) '~/.config/{0}'.format(i)])
+#
+# for i in exec_files:
+#     run(['ln', '-srf', './scripts/{0}'.format(i), '/bin/{0}'.format(i)])
 
-for i in exec_files:
-    run(['ln', '-srf', './scripts/{0}'.format(i), '/bin/{0}'.format(i)])
+print(dot_files)
+
+for i in dot_files:
+    run(['ln', '-srf', './{0}'.format(i), '~/'.format(i)])
